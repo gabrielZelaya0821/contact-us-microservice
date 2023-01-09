79ef3e3bcc28150404ce3750998a881b12a8c590
gabrielZelaya0821 <115841923+gabrielZelaya0821@users.noreply.github.com>
2023-01-09 14:25:38 -0600

    Add files via upload

---
+++ contact-us.zip_expanded/contact-us/src/main/java/com/baas/controller/ClientController.java
@@ -0,0 +1,43 @@
+package com.baas.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baas.model.Client;
+import com.baas.service.ClientService;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+	@Autowired 
+	private ClientService clientService;
+	
+	@GetMapping("/id/{id}")
+	public Client getById(@PathVariable("id") String clientId) {
+		return clientService.getById(clientId);
+	}
+	
+	@PostMapping("/save")
+	public Client saveClient(@RequestBody Client client) {
+		return clientService.saveClient(client);
+	}
+	
+	@PutMapping("/update")
+	public Client updateClient(@RequestBody Client client) {
+		return clientService.saveClient(client);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String delete(@PathVariable("id") String clientId) {
+		clientService.deleteById(clientId);
+		return "ID:" + clientId + " deleted";
+	}
+}
