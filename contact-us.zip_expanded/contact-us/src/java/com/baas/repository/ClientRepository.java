7f06a8a138af87cefa3c049fa9cc737d36a1045e
gabrielZelaya0821 <115841923+gabrielZelaya0821@users.noreply.github.com>
2023-01-11 16:36:56 -0600

    Delete contact-us.zip_expanded/contact-us/src directory

--- contact-us.zip_expanded/contact-us/src/java/com/baas/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baas.repository;
-
-import com.baas.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, String>{
-	Client getById(String clientId);
-}
