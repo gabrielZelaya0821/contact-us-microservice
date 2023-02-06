af555112ecd3c9b18855e0cfc289903738e00b51
gabrielZelaya0821 <115841923+gabrielZelaya0821@users.noreply.github.com>
2023-02-06 13:38:09 -0600

    Delete contact-us.zip_expanded directory

--- contact-us.zip_expanded/contact-us/src/main/java/com/baas/ThrowException/ClientNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.baas.ThrowException;
-
-@SuppressWarnings("serial")
-public class ClientNotFoundException extends Exception{
-	
-	public ClientNotFoundException(String message) {
-		super(message);
-	}
-}
